e6c0f9ab76a2cf0c5daa0687dad8707eb1cbb40a
Jeff Dauda <j.dauda@alustudent.com>
2023-02-25 03:16:05 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import json
+import uuid
+from basicauth import encode
+
+class Payment():
+    # variables and keys
+
+    # collections subscription key
+    collections_subkey = ""
+
+    # disbursements subscription key
+    disbursement_subkey = ""
+
+    # Production collections basic auth key
+    basic_auth_collections = ""
+
+    
+
+
